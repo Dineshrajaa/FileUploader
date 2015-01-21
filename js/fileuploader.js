b1e9618410ae0288573b0f166883a7becd7948ba
Dineshrajaa <dineshrajaa.93@gmail.com>
2015-01-21 10:04:48 +0530

    Completed the Requirement

--- js/fileuploader.js
+++ js/fileuploader.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){	
+	var picArray=[];
 			$("#picker").change(function(event){
 				//When the user picks an file the Change event is triggered
 			var picPath=URL.createObjectURL(event.target.files[0]);
-			$("#imageBanner").append("<img src='"+picPath+"' class='piclass'/>");//Print the Picture in the Imagebanner.
+			picArray.push(picPath);
+			console.log(picArray[0]);
+			$("#imageBanner").html("");
+			for(var i=0;i<picArray.length;i++){
+				$("#imageBanner").append("<img src='"+picArray[i]+"' class='piclass'/>");//Print the Picture in the Imagebanner.
+			}
+			//$("#imageBanner").append("<img src='"+picPath+"' class='piclass'/>");//Print the Picture in the Imagebanner.
 			//You have to handle the picPath to store it in an array
 		});
 	});
